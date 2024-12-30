dd7ecca9f8bb8fafb4528065a7e09b780e19f18e
EunSeop <doosan1112@naver.com>
2024-12-30 23:33:22 +0900

    feat: 초기설정

---
+++ src/main/java/com/example/thedayoftoday/ThedayoftodayApplication.java
@@ -0,0 +1,13 @@
+package com.example.thedayoftoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedayoftodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThedayoftodayApplication.class, args);
+	}
+
+}
