3de5fdbba64b2404e523f28c6766a64b85349ca1
EunSeop <doosan1112@naver.com>
2025-02-19 23:27:15 +0900

    fix:파일 이름 변경

--- src/main/java/com/example/thedayoftoday/domain/repository/SentimentalAnalysisRepository.java
+++ src/main/java/com/example/thedayoftoday/domain/repository/SentimentalAnalysisRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SentimentAnalysisRepository extends JpaRepository<SentimentalAnalysis, Long> {
+public interface SentimentalAnalysisRepository extends JpaRepository<SentimentalAnalysis, Long> {
 
 }
