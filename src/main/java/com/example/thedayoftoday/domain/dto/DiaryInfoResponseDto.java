0d93e52953c5bd1ca680cc0398d76331436d41e1
Hun0914 <bkh010914@naver.com>
2025-03-19 21:31:04 +0900

    fix : 회원가입 DTO 수정

--- src/main/java/com/example/thedayoftoday/domain/dto/DiaryInfoResponseDto.java
+++ src/main/java/com/example/thedayoftoday/domain/dto/DiaryInfoResponseDto.java
@@ -4,7 +4,7 @@ import com.example.thedayoftoday.domain.entity.DiaryMood;
 import java.time.LocalDateTime;
 //늘 moodMeter, moodColor은 AI가 넣는것이 아닌 사람이 직접 주입
 public record DiaryInfoResponseDto(
-        String userName,
+        String name,
         String title,
         String content,
         LocalDateTime createTime,
--- src/main/java/com/example/thedayoftoday/domain/dto/SignupRequestDto.java
+++ src/main/java/com/example/thedayoftoday/domain/dto/SignupRequestDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignupRequestDto(
-        @NotBlank(message = "닉네임은 필수 입력 값입니다.") String nickname,
         @NotBlank(message = "이름은 필수 입력 값입니다.") String name,
         @Email(message = "올바른 이메일 형식이어야 합니다.")
         @NotBlank(message = "이메일은 필수 입력 값입니다.") String email,
