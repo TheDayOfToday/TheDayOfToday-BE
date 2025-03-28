e82fdfec27bb8d37fd6354d543324aaf32caa3cd
Hun0914 <bkh010914@naver.com>
2025-03-28 15:47:20 +0900

    fix : WeeklyData의 MoodMeter을 Degree로 변경

---
+++ src/main/java/com/example/thedayoftoday/domain/dto/WeeklyTitleFeedbackResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.thedayoftoday.domain.dto;
+
+public class WeeklyTitleFeedbackResponseDto {
+}
---
+++ src/main/java/com/example/thedayoftoday/domain/scheduler/WeeklySummaryScheduler.java
@@ -0,0 +1,4 @@
+package com.example.thedayoftoday.domain.scheduler;
+
+public class WeeklySummaryScheduler {
+}
