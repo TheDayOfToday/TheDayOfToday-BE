8f7cffc259d8c30450afe24adebe3db10ef02e4e
Hun0914 <bkh010914@naver.com>
2025-04-08 19:18:13 +0900

    refactor : 회원가입 및 회원정보 조회 EndPoint 변경 및 리턴값 변경

--- src/main/java/com/example/thedayoftoday/app/UserController.java
+++ src/main/java/com/example/thedayoftoday/app/UserController.java
@@ -1,7 +1,7 @@
 package com.example.thedayoftoday.app;
 
 import com.example.thedayoftoday.domain.dto.login.signup.SignupRequestDto;
-import com.example.thedayoftoday.domain.dto.setting.UserSettingDto;
+import com.example.thedayoftoday.domain.dto.setting.UserInfoDto;
 import com.example.thedayoftoday.domain.entity.User;
 import com.example.thedayoftoday.domain.repository.UserRepository;
 import com.example.thedayoftoday.domain.security.CustomUserDetails;
@@ -29,16 +29,16 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/default")
-    public ResponseEntity<UserSettingDto> getSetting(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    @GetMapping("/info")
+    public ResponseEntity<UserInfoDto> getSetting(@AuthenticationPrincipal CustomUserDetails userDetails) {
 
         Long userId = userDetails.getUserId();
         User user = userRepository.findById(userId).orElse(null);
         if (user == null) {
             throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
         }
-        UserSettingDto userSettingDto = new UserSettingDto(user.getName(), user.getEmail(), user.getPhoneNumber());
-        return ResponseEntity.ok(userSettingDto);
+        UserInfoDto userInfoDto = new UserInfoDto(userId, user.getName(), user.getEmail(), user.getPhoneNumber());
+        return ResponseEntity.ok(userInfoDto);
     }
 
     @PutMapping("/update-password")
@@ -56,8 +56,8 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Long> addUser(@Valid @RequestBody SignupRequestDto signupRequestDto) {
-        Long id = userService.join(signupRequestDto);
-        return ResponseEntity.ok(id);
+    public ResponseEntity<String> addUser(@Valid @RequestBody SignupRequestDto signupRequestDto) {
+        userService.join(signupRequestDto);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
 }
