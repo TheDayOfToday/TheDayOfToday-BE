662593ad159469d575a27673b55ba8584ec1d56d
Hun0914 <bkh010914@naver.com>
2025-04-09 21:26:10 +0900

    refactor : password 암호화해서 DB 저장

--- src/main/java/com/example/thedayoftoday/app/UserController.java
+++ src/main/java/com/example/thedayoftoday/app/UserController.java
@@ -9,6 +9,7 @@ import com.example.thedayoftoday.domain.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,10 +23,12 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository, UserService userService) {
+    public UserController(UserRepository userRepository, UserService userService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/info")
@@ -45,14 +48,18 @@ public class UserController {
                                                  @RequestBody String newPassword) {
         Long userId = userDetails.getUserId();
         User user = userRepository.findById(userId).orElse(null);
+
         if (user == null) {
             throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
         }
-        if (user.getPassword().equals(newPassword)) {
+        if (passwordEncoder.matches(newPassword, user.getPassword())) {
             throw new IllegalArgumentException("기존의 비밀번호와 동일합니다.");
         }
-        user.setPassword(newPassword);
+
+        String encodedPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(encodedPassword);
         userRepository.save(user);
+
         return ResponseEntity.ok("비밀변호 변겅이 완료되었습니다.");
     }
 
