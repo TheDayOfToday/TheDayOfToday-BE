2985a230c81849dfe61398240ae1869bea984d44
Hun0914 <bkh010914@naver.com>
2025-04-11 15:57:23 +0900

    refactor : 미비된 트랜잭션 어노테이션들 수정

--- src/main/java/com/example/thedayoftoday/domain/scheduler/WeeklySummaryScheduler.java
+++ src/main/java/com/example/thedayoftoday/domain/scheduler/WeeklySummaryScheduler.java
@@ -15,9 +15,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class WeeklySummaryScheduler {
 
     private final WeeklyAnalysisService weeklyAnalysisService;
