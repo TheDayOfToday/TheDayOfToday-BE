ba569c990256315604810854fbbc52b922f38aa3
EunseopKim <eunseop@EunseopKimui-MacBookAir.local>
2025-05-02 22:53:26 +0900

    도서추천 컨트롤러 생성

--- src/main/java/com/example/thedayoftoday/app/BookController.java
+++ src/main/java/com/example/thedayoftoday/app/BookController.java
@@ -1,9 +1,31 @@
 package com.example.thedayoftoday.app;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.thedayoftoday.domain.entity.Diary;
+import com.example.thedayoftoday.domain.repository.DiaryRepository;
+import com.example.thedayoftoday.domain.security.CustomUserDetails;
+import com.example.thedayoftoday.domain.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/book")
 public class BookController {
+
+    private final BookService bookService;
+    private final DiaryRepository diaryRepository;
+
+    @PostMapping("/recommend")
+    public ResponseEntity<Void> recommendBook(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam("diaryId") Long diaryId
+    ) {
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(() -> new IllegalArgumentException("일기를 찾을 수 없습니다."));
+
+        bookService.recommendBook(userDetails.getUserId(), diary.getContent());
+        return ResponseEntity.ok().build();
+    }
 }
