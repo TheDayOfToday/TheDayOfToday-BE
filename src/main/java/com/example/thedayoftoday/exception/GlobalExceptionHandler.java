44d526c0e572d761d31193b4bf2d330286d5ec70
Hun0914 <bkh010914@naver.com>
2025-05-09 00:03:57 +0900

    feat : 전역변수 핸들러에 중복 Email 처리 로직 추가

--- src/main/java/com/example/thedayoftoday/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/thedayoftoday/exception/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.example.thedayoftoday.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailDuplicationException.class)
+    public ResponseEntity<String> handleEmailDuplicate(EmailDuplicationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
 }
