777434a6eaab5ebcf8e492c39f75ec0ab10e6080
Hun0914 <bkh010914@naver.com>
2025-05-09 10:50:31 +0900

    feat : Valid 어노테이션 전용 전역 예외 핸들러 작성

--- src/main/java/com/example/thedayoftoday/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/thedayoftoday/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.thedayoftoday.exception;
 
+import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailDuplicationException.class)
-    public ResponseEntity<String> handleEmailDuplicate(EmailDuplicationException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ErrorCodeMessage> handleEmailDuplicate(EmailDuplicationException e) {
+        ErrorCodeMessage errorCodeMessage = new ErrorCodeMessage(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorCodeMessage);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorCodeMessage> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .findFirst().orElse("잘못된 요청입니다");
+        ErrorCodeMessage errorCodeMessage = new ErrorCodeMessage(message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorCodeMessage);
+    }
+
 
 }
