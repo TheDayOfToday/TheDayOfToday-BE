8cab8767556981d667b0114d73c179640e8af0b7
Hun0914 <bkh010914@naver.com>
2025-05-09 14:28:38 +0900

    feat : 중복 전화번호 발견 시 예외처리 추가

--- src/main/java/com/example/thedayoftoday/domain/service/UserService.java
+++ src/main/java/com/example/thedayoftoday/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.thedayoftoday.domain.service;
 import com.example.thedayoftoday.domain.dto.user.SignupRequestDto;
 import com.example.thedayoftoday.domain.entity.User;
 import com.example.thedayoftoday.domain.entity.enumType.RoleType;
+import com.example.thedayoftoday.domain.exception.PhoneNumberDuplicationExceptiono;
 import com.example.thedayoftoday.domain.repository.UserRepository;
 import com.example.thedayoftoday.domain.exception.EmailDuplicationException;
 import jakarta.validation.Valid;
@@ -24,10 +25,9 @@ public class UserService {
 
     public Long join(@Valid SignupRequestDto user) {
         boolean validUser = userRepository.existsByEmail(user.email());
+        boolean validPhoneNumber = userRepository.existsByPhoneNumber(user.phoneNumber());
 
-        if (validUser) {
-            throw new EmailDuplicationException("해당 Email은 이미 존재합니다.");
-        }
+        validateDuplicate(validUser, validPhoneNumber);
 
         User newUser = User.builder()
                 .name(user.name())
@@ -39,6 +39,15 @@ public class UserService {
 
         User savedUser = userRepository.save(newUser);
         return savedUser.getUserId();
+    }
+
+    private static void validateDuplicate(boolean validUser, boolean validPhoneNumber) {
+        if (validUser) {
+            throw new EmailDuplicationException("해당 Email은 이미 존재합니다.");
+        }
+        if(validPhoneNumber){
+            throw new PhoneNumberDuplicationExceptiono("해당 전화번호는 이미 존재합니다.");
+        }
     }
 
     public Optional<User> findById(Long userId) {
